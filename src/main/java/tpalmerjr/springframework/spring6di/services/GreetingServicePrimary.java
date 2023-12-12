85a8e3aeef535a0c223783566460d07f8a69c068
Terry Palmer <tpalmerjr@gmail.com>
2023-12-12 17:20:09 -0600

    6-primary-bean

---
+++ src/main/java/tpalmerjr/springframework/spring6di/services/GreetingServicePrimary.java
@@ -0,0 +1,14 @@
+package tpalmerjr.springframework.spring6di.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class GreetingServicePrimary implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello from the Primary Bean!!";
+    }
+}

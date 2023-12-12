dbcab2cd3b69ffb03dc9393077a6fbc27689d3f9
Terry Palmer <tpalmerjr@gmail.com>
2023-12-12 13:46:38 -0600

    1-spring-context

---
+++ src/main/java/tpalmerjr/springframework/spring6di/Spring6DiApplication.java
@@ -0,0 +1,20 @@
+package tpalmerjr.springframework.spring6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import tpalmerjr.springframework.spring6di.controllers.MyController;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+        MyController controller = ctx.getBean(MyController.class);
+
+        System.out.println("In Main Method");
+
+        System.out.println(controller.sayHello());
+    }
+}

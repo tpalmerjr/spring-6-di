8054f4cd2f2560111c7e00ffd23dd4318268b233
Terry Palmer <tpalmerjr@gmail.com>
2023-12-12 13:47:19 -0600

    2-spring-test-context

---
+++ src/test/java/tpalmerjr/springframework/spring6di/Spring6DiApplicationTests.java
@@ -0,0 +1,34 @@
+package tpalmerjr.springframework.spring6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import tpalmerjr.springframework.spring6di.controllers.MyController;
+
+@SpringBootTest
+class Spring6DiApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowireOfController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromCtx() {
+        MyController myController = applicationContext.getBean(MyController.class);
+
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+}
